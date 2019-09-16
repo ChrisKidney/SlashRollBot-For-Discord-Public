a0efafa1cdc8467791fca818c697a2f630d424bf
ChrisKidney <christopherkidney@hotmail.com>
2019-09-16 01:34:26 -0300

    Added dice roll functionality

--- bot.js
+++ bot.js
@@ -29,6 +29,10 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; 
 }
+// Function used to get sum of array with reduce()
+function getsum(total, num){
+    return total + num;
+}
 
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Grab username for output
@@ -41,6 +45,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         var x = args[1];
         var y = args[2];
         var z = args[3];
+        var a = args[4];
+        var b = args[5];
        
         args = args.splice(1);
         switch(cmd) {
@@ -52,14 +58,14 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         to: channelID,
                         message: '**' + user + '**' + ' rolled: **' + result +'**'
                     });
-                     break;
+                    break;
                 // !roll help : displays help text
-                } else if( x == 'help'){
+                }else if( x == 'help'){
                      bot.sendMessage({
                         to: channelID,
-                        message: '``` SlashRollBot Commands: \n \n !roll : rolls between 1 and 100 \n \n !roll # : rolls between 1 and # \n \n !roll # # : rolls between # and # \n \n !roll help : Displays this help text```'
+                        message: '``` SlashRollBot Commands: \n \n Wow-like roll commands: \n \n !roll : rolls between 1 and 100 \n \n !roll # : rolls between 1 and # \n \n !roll # # : rolls between # and # \n \n !roll help : Displays this help text \n \n Dice Roll Commands: \n \n !roll d# : rolls # sided dice \n \n !roll #1 d#2 : rolls #1 amount of #2 sided dice \n \n !roll d#1 + #2 : rolls #1 sided dice and adds #2. "+" can be substituted for -, *, x or / for different operations. \n \n !roll #1 d#2 + #3 : rolls #1 amount of #2 sided dice and adds #3, "+" can be substituted, same as above```'
                     });
-                      break;
+                     break;
                 // !roll # # : rolls between # and #     
                 }else if(x >= 0 && y >= 0 && z == null){ 
                     var result = getRandomInt(x,y);
@@ -67,7 +73,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         to: channelID,
                         message: '**' + user + '**' + ' rolled: **' + result +'**'
                     });
-                     break;
+                    break;
                 // !roll # : rolls between 1 and #
                 }else if(x >= 0 && y == null){
                     var result = getRandomInt(1,x);
@@ -75,7 +81,118 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         to: channelID,
                         message: '**' + user + '**' + ' rolled: **' + result +'**'
                     }); 
-                     break;
+                    break;
+                // !roll d# : rolls (#) sided dice
+                }else if((x.substring(0,1) === "d" || x.substring(0,1) === "D") && y == null){
+                    var dice = parseInt(x.substring(1),10);
+                    var result = getRandomInt(1,dice);
+                    if(dice == 20 && result == 20){
+                        bot.sendMessage({
+                            to: channelID,
+                            message: '**' + user + '**' + ' rolled: **' + result +'**' + ' ***' + "  Natural 20! Critical Success! :)" + '***'
+                        }); 
+                        break;
+                    }else if(dice == 20 && result == 1){
+                        bot.sendMessage({
+                            to: channelID,
+                            message: '**' + user + '**' + ' rolled: **' + result +'**' + ' ***' + "  Natural 1, Critical Failure.. :(" + '***'
+                        }); 
+                        break;
+                    }else{
+                        bot.sendMessage({
+                            to: channelID,
+                            message: '**' + user + '**' + ' rolled: **' + result +'**'
+                        }); 
+                        break;
+                    }
+                     break;  
+                // !roll x d# : rolls a user specified amount (x) of (#) sided dice
+                }else if(x >= 0 && (y.substring(0,1) === "d" || y.substring(0,1) === "D") && z == null){
+                    var amount = x;
+                    var diceArray = [];
+                    var output ="";             
+                    var dice = parseInt(y.substring(1),10);
+                    for(i=0; i < amount; i++){
+                        diceArray[i] = getRandomInt(1,dice);
+                        
+                        output += String(diceArray[i]) + " ";
+
+                        if(i != (amount-1)){
+                            output+="+ ";
+                        }else{
+                            output+="= ";
+                        }   
+                    }           
+                    sum = diceArray.reduce(getsum);      
+                    bot.sendMessage({
+                        to: channelID,
+                        message: '**' + user + '**' + ' rolled: **' + output +'** **' + sum + '**'
+                    }); 
+                    break;
+                // !roll d# (+,-,*,x,/) y : rolls a (#) sided dice and adds, subtracts, divides, or multiplies by y basd on users operator input
+                }else if((x.substring(0,1) === "d" || x.substring(0,1) === "D") && y != null && z >= 0 && a == null){
+                    var dice = parseInt(x.substring(1),10);
+                    var result = getRandomInt(1,dice);
+                    var output = "( " + result; 
+
+                    if(y === "+"){
+                        output += " ) + " + z + " =";
+                        result = parseFloat(result)+parseFloat(z);
+                    }else if(y === "-"){
+                        output += " ) - " + z + " =";
+                        result = parseFloat(result)-parseFloat(z);
+                    }else if(y === "x" || y === "*"){
+                        output += " ) x " + z + " =";
+                        result = parseFloat(result)*parseFloat(z);
+                    }else if(y === "/"){
+                        output += " ) / " + z + " =";
+                        result = parseFloat(result)/parseFloat(z);
+                    }else{
+                        break;
+                    } 
+
+                    bot.sendMessage({
+                        to: channelID,
+                        message: '**' + user + '**' + ' rolled: **' + output +'** **' + result + '**'
+                    }); 
+                    break;
+                // !roll x d# (+,-,*,x,/) y : rolls a user specified amount (x) of (#) sided dice and adds, subtracts, divides, or multiplies by y based on users operator input
+                }else if((x >= 0 && (y.substring(0,1) === "d" || y.substring(0,1) === "D") && z != null && a >= 0 && b == null)){
+                    var amount = x;
+                    var value = a;
+                    var diceArray = [];
+                    var output ="( ";             
+                    var dice = parseInt(y.substring(1),10);
+                    for(i=0; i < amount; i++){
+                        diceArray[i] = getRandomInt(1,dice);
+                        
+                        output += String(diceArray[i]) + "";
+
+                        if(i != (amount-1)){
+                            output+=" + ";
+                        }
+                    }           
+                    sum = diceArray.reduce(getsum);  
+                    if(z === "+"){
+                        output += " ) + " + a + " =";
+                        sum = parseFloat(sum)+parseFloat(a);
+                    }else if(z === "-"){
+                        output += " ) - " + a + " =";
+                        sum = parseFloat(sum)-parseFloat(a);
+                    }else if(z === "x" || z === "*"){
+                        output += " ) x " + a + " =";
+                        sum = parseFloat(sum)*parseFloat(a);
+                    }else if(z === "/"){
+                        output += " ) / " + a + " =";
+                        sum = parseFloat(sum)/parseFloat(a);
+                    }else{
+                        break;
+                    }    
+                    bot.sendMessage({
+                        to: channelID,
+                        message: '**' + user + '**' + ' rolled: **' + output +'** **' + sum + '**'
+                    }); 
+                    break;       
                 }else{
                     break;
                 }
